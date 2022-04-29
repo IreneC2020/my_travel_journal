0703e2093e7480d31ef0e8f39c29c1fb2794ef86
Irene C <irenec0101@outlook.com>
2022-04-28 17:42:05 -0700

    modified css

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,21 @@
 export default function Card(props){
     return (
         <div className="card">
-            <img src = {props.item.imageUrl} alt = "card-img" className="card-img"/>
-            <p className="card-location">{props.item.location}</p>
-            <h1 className="card-title">{props.item.title}</h1>
-            <h2 className="date">{props.item.startDate} - {props.item.endDate}</h2>
-            <p className="card-description">{props.item.description}</p>
+            <div className="card-div-img">
+                <img src = {props.item.imageUrl} alt = "card-img" className="card-img"/>
+            </div>
+            <div className="card-div-text">
+            <div className="card-map">
+                 
+                <p className="card-location">
+                    {props.item.location}
+                    <a href={props.item.googleMapsUrl}> view on google map</a>
+                </p>
+            </div>
+                <h1 className="card-title">{props.item.title}</h1>
+                <h2 className="date">{props.item.startDate} - {props.item.endDate}</h2>
+                <p className="card-description">{props.item.description}</p>
+            </div>
         </div>
     )
 }
